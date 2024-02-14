8577c33e65795714746d947c91792379750c339a
meysam zohouri <meysamzohoori.mehriz@gmail.com>
2024-02-14 14:23:07 +0330

    feat: welcome route

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(router *gin.Engine) {
 	auth := router.Group("/auth")
@@ -11,6 +13,10 @@ func RegisterRoutes(router *gin.Engine) {
 	user := router.Group("/user")
 	{
 		user.POST("/register", RegisterHandler)
+	}
+	admin := router.Group("/admin")
+	{
+		admin.GET("/welcome", WelcomeHandler())
 	}
 
 }

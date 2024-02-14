bc093d6b59039c4e58bcce50df0bd6e525c1b07e
meysam zohouri <meysamzohoori.mehriz@gmail.com>
2024-02-14 12:04:30 +0330

    initial commi

---
+++ internal/handlers/auth_handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"jwt-example-go/db"
+	auth "jwt-example-go/internal/auth/jwt"
+	"jwt-example-go/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LoginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func LoginHandler(c *gin.Context) {
+	var req LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user models.User
+	if err := db.GetDB().Where("username = ?", req.Username).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	if user.Password != req.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token, err := auth.GenerateToken(user.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+
+}
---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"jwt-example-go/config"
+	auth "jwt-example-go/internal/auth/jwt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(cfg *config.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token not provided"})
+			c.Abort()
+			return
+		}
+
+		claims, err := auth.VerifyToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("username", claims.Username)
+
+		c.Next()
+	}
+}
